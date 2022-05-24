88f83e74e1e34850f214b67984ed60a4097931f7
Juan R Cardenas C <cjuanronal@gmail.com>
2022-05-23 20:31:08 -0500

    Coreccion de sale

--- src/main/java/com/compusac/models/entity/Sale.java
+++ src/main/java/com/compusac/models/entity/Sale.java
@@ -2,13 +2,18 @@ package com.compusac.models.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity()
+@Entity
 @Table(name = "sales")
 public class Sale {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private Long product;
+	private int product;
 	@Column(name = "unit_price")
 	private double unitPrice;
 	private Integer quantity;
@@ -16,46 +21,61 @@ public class Sale {
 	private double totalPrice;
 	private Integer user;
 	private String date;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public Long getProduct() {
+
+	public int getProduct() {
 		return product;
 	}
-	public void setProduct(Long product) {
+
+	public void setProduct(int product) {
 		this.product = product;
 	}
+
 	public double getUnitPrice() {
 		return unitPrice;
 	}
+
 	public void setUnitPrice(double unitPrice) {
 		this.unitPrice = unitPrice;
 	}
+
 	public Integer getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
 	}
+
 	public double getTotalPrice() {
 		return totalPrice;
 	}
+
 	public void setTotalPrice(double totalPrice) {
 		this.totalPrice = totalPrice;
 	}
+
 	public Integer getUser() {
 		return user;
 	}
+
 	public void setUser(Integer user) {
 		this.user = user;
 	}
+
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
 }
