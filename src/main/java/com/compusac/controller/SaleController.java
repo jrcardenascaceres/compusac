fb3dc8124672f6b6ec020fb9a19d47bce6762c24
Jonathan Reyna <jonathan.reyna@onroadts.com>
2022-05-25 22:06:58 -0500

    se completo eliminar registro

--- src/main/java/com/compusac/controller/SaleController.java
+++ src/main/java/com/compusac/controller/SaleController.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.compusac.models.entity.Sale;
 import com.compusac.models.service.IProductService;
@@ -64,6 +66,14 @@ public class SaleController {
 		model.addAttribute("totalVenta", salesService.total(1));
 		
 		return "shopping-cart";
+	}
+	
+	@GetMapping("/delete")
+	public String eliminar(@RequestParam Long id) {
+		salesService.delete(id);
+		//model.addAttribute("status", true);
+
+		return "redirect:/shopping-cart";
 	}
 
 }
